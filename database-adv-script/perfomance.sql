ef547176c575d0c334b1fe7cd0cf934007210e28
Tesfamariam T <51912936+Wege0921@users.noreply.github.com>
2025-07-06 11:08:07 +0300

    Create perfomance.sql

---
+++ database-adv-script/perfomance.sql
@@ -0,0 +1,15 @@
+-- Initial unoptimized query to retrieve all bookings with user, property, and payment details
+
+-- BEFORE Optimization
+EXPLAIN ANALYZE
+SELECT
+    b.id AS booking_id,
+    u.name AS user_name,
+    p.name AS property_name,
+    pay.amount AS payment_amount,
+    pay.status AS payment_status
+FROM
+    bookings b
+JOIN users u ON b.user_id = u.id
+JOIN properties p ON b.property_id = p.id
+JOIN payments pay ON b.id = pay.booking_id;
