8aa4339aebcf591affce3eb2e716a1338ab6defe
Tesfamariam T <51912936+Wege0921@users.noreply.github.com>
2025-07-06 11:09:53 +0300

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -13,3 +13,18 @@ FROM
 JOIN users u ON b.user_id = u.id
 JOIN properties p ON b.property_id = p.id
 JOIN payments pay ON b.id = pay.booking_id;
+
+
+-- AFTER Optimization: Assumes appropriate indexes exist
+EXPLAIN ANALYZE
+SELECT
+    b.id AS booking_id,
+    u.name AS user_name,
+    p.name AS property_name,
+    pay.amount AS payment_amount,
+    pay.status AS payment_status
+FROM
+    bookings b
+JOIN users u ON b.user_id = u.id
+JOIN properties p ON b.property_id = p.id
+JOIN payments pay ON pay.booking_id = b.id;
